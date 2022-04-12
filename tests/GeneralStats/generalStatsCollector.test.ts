1e4d7838761f9bbc4a9d6c11fda903c8c32aad02
Yukon Chen <yukonchen@microsoft.com>
2022-04-12 11:23:52 -0700

    Remove old test, add CI run to test

--- tests/GeneralStats/generalStatsCollector.test.ts
+++
@@ -1,6 +0,0 @@
-import * as generalStatsCollector from '../../src/GeneralStats/generalStatsCollector'
-
-test('3 to equal 3', () => {
-  const callStats = generalStatsCollector
-  expect(callStats).toBeUndefined()
-})
