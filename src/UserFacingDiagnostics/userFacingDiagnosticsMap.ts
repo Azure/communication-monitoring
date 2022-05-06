ffe9f27899129c88568690df49c8c50a1cd736f9
Jorge Beauregard <jbeauregardb@microsoft.com>
2022-05-05 17:30:12 -0700

    Color coding fix

---
+++ src/UserFacingDiagnostics/userFacingDiagnosticsMap.ts
@@ -0,0 +1,59 @@
+export const UserFacingDiagnosticsMap = {
+  noNetwork: {
+    reverseColors: true,
+  },
+  networkRelaysNotReachable: {
+    reverseColors: true,
+  },
+  networkReconnect: {
+    reverseColors: true,
+  },
+  networkReceiveQuality: {
+    reverseColors: false,
+  },
+  networkSendQuality: {
+    reverseColors: false,
+  },
+  noSpeakerDevicesEnumerated: {
+    reverseColors: true,
+  },
+  speakingWhileMicrophoneIsMuted: {
+    reverseColors: true,
+  },
+  noMicrophoneDevicesEnumerated: {
+    reverseColors: true,
+  },
+  microphoneNotFunctioning: {
+    reverseColors: true,
+  },
+  microphoneMuteUnexpectedly: {
+    reverseColors: true,
+  },
+  microphonePermissionDenied: {
+    reverseColors: true,
+  },
+  cameraFreeze: {
+    reverseColors: true,
+  },
+  cameraStartFailed: {
+    reverseColors: true,
+  },
+  cameraStartTimedOut: {
+    reverseColors: true,
+  },
+  cameraPermissionDenied: {
+    reverseColors: true,
+  },
+  cameraStoppedUnexpectedly: {
+    reverseColors: true,
+  },
+  screenshareRecordingDisabled: {
+    reverseColors: true,
+  },
+  capturerStartFailed: {
+    reverseColors: true,
+  },
+  capturerStoppedUnexpectedly: {
+    reverseColors: true,
+  },
+}
